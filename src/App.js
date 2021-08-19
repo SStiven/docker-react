b3f5f1a8611fa2e2fe06e1f6298443febec9d6ac
Stiven Villegas <sstiven@protonmail.com>
2021-08-19 17:38:24 -0500

    Agrega consumo de servicio en el backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
 
 function App() {
+  let requestOptions = {
+    method: 'GET'
+  };
+  
+  fetch("http://localhost:9000", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+    
   return (
     <div className="App">
       Prueba
